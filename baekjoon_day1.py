d9bbe96e2c9712681407df003df53eabbea333db
swChoi <chsw030818@naver.com>
2025-07-21 23:25:46 +0900

    새싹

---
+++ baekjoon_day1.py
@@ -0,0 +1,9 @@
+#print("         ,r\'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |")
+
+# piece_cnt = [1, 1, 2, 2, 2, 8]
+
+# piece = list(map(int, input().split()))
+
+# for i in piece_cnt:
+
+
