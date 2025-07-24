f0ce9065f06e84cc9b7d28367fc770ca08099a6d
swChoi <chsw030818@naver.com>
2025-07-25 00:09:11 +0900

    시간복잡도

---
+++ baekjoon_day3.py
@@ -0,0 +1,10 @@
+# def MenOfPassion(A, n):
+#     sum = 0
+#     for i in range(1, n):
+#         sum += A[i] # 코드1
+#     return sum
+# n = int(input())
+# print(n,"\n1")
+
+n = int(input())
+print(n**2,"\n2")
