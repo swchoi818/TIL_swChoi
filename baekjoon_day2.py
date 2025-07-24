d6c2e35a8fb43bc9929a86e51ba3f8680917e3fc
swChoi <chsw030818@naver.com>
2025-07-24 09:25:39 +0900

    소인수분해 수정

--- baekjoon_day2.py
+++ baekjoon_day2.py
@@ -1,27 +1,12 @@
-N = 1000000
-isPrime = [True]*N
-isPrime[0] = False
-isPrime[1] = False
-for i in range(2, int(N**0.5) + 1):
-    if isPrime[i]:
-        for j in range(i + i, N, i):
-            isPrime[j] = False
-
 factor = []
 
 def primeFactor(a):
-    if isPrime[a]:
-        factor.append(a)
-        return
-    for i in range(2, a):
+    for i in range(2, a + 1):
         if a%i == 0:
             factor.append(i)
             j = a//i
-            if isPrime[j]:
-                factor.append(j)
-            else:
-                primeFactor(j)
-            return
+            return primeFactor(j)
+            
 
 n = int(input())
 primeFactor(n)
