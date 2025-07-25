d57dfd912324be9e7696ff5e81de6c30948df71b
swChoi <chsw030818@naver.com>
2025-07-25 16:55:27 +0900

    시간복잡도6 진행 중

--- baekjoon_day3.py
+++ baekjoon_day3.py
@@ -6,5 +6,20 @@
 # n = int(input())
 # print(n,"\n1")
 
-n = int(input())
-print(n**2,"\n2")
+# n = int(input())
+# print(n**2,"\n2")
+
+# n = int(input())
+# print(int(n*(n-1)/2),"\n2")
+def MenOfPassion(n) :
+    count = 0
+    for i in range(1, n - 2):
+        for j in range(i + 1, n - 1):
+            for k in range(j + 1, n):
+                count += 1; # 코드1
+    return count
+
+
+print(MenOfPassion(int(input())),"\n3")
+
+# (n-2)(n-1) *n//6
